a009e5d943819bdfe772dd4c6e04fc2ab7fb8b56
Benjamin White <benjamin.malilay.white@gmail.com>
2018-05-21 20:53:26 -0500

    additional tests, fixes to pass R CMD check

--- tests/testthat/test_io.R
+++ tests/testthat/test_io.R
@@ -2,8 +2,21 @@ context("get_porp()")
 library(draftr)
 
 test_that("Output format is correct", {
+  #whole output
   expect_output(str(get_porp(bball)), "List of 2")
+  #df
   expect_is(get_porp(bball)$df, "data.frame")
+  #chart
   expect_is(get_porp(bball)$chart, c("gg", "ggplot"))
+  #same number of players in both input and output
   expect_equal(nrow(bball), nrow(get_porp(bball)$df))
+})
+
+test_that("Input error handling is correct", {
+  #non data frame input
+  expect_error(get_porp(3))
+  #appropriate variable names missing
+  expect_error(get_porp(unname(bball)))
+  #remaining columns that are numeric projections
+  expect_error(get_porp(cbind(bball, "I am not a fantasy projection")))
 })
